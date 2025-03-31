7346dd3fa10d7dcfc4bf313c7141c8175eb1d16b
Patrik <Muravski.patrik@gmail.com>
2025-03-31 16:07:42 +0300

    Seeded the roles

--- MutipleStoreWebApp/Data/ApplicationDbContext.cs
+++ MutipleStoreWebApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
@@ -20,14 +21,36 @@ namespace MutipleStoreWebApp.Data
         }
 
         // (global query filter) to enforce store isolation at the database level.
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
 
+            // seeding
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "f64f1b15-0d0c-4b20-997c-4649d9d31b64",
+                    Name = Static.Roles.Admin,
+                    NormalizedName = Static.Roles.Admin.ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = "1d102caa-dcd1-4c0d-ac38-76e2b8b04f33",
+                    Name = Static.Roles.Owner,
+                    NormalizedName = Static.Roles.Owner.ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = "fc57d154-917f-4485-9f9d-2f745c3490f9",
+                    Name = Static.Roles.Customer,
+                    NormalizedName = Static.Roles.Customer.ToUpper()
+                });
+
+            // global query
             var user = _httpContextAccessor.HttpContext?.User;
             var userEmail = user?.FindFirst(ClaimTypes.Email)?.Value;
 
-            modelBuilder.Entity<Product>().HasQueryFilter(p => userEmail == null || p.Store.OwnerEmail == userEmail);
+            builder.Entity<Product>().HasQueryFilter(p => userEmail == null || p.Store.OwnerEmail == userEmail);
         }
     }
 }
---
+++ MutipleStoreWebApp/Static/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace MutipleStoreWebApp.Static
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Owner = "Owner";
+        public const string Customer = "Customer";
+    }
+}
