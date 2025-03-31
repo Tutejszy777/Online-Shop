543eb1eb8281a0f01f03f944a5ef50bec2396101
Patrik <Muravski.patrik@gmail.com>
2025-03-31 11:24:55 +0300

    updated migrations, choosed approriate connection string

--- MutipleStoreWebApp/Program.cs
+++ MutipleStoreWebApp/Program.cs
@@ -5,7 +5,7 @@ using MutipleStoreWebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
