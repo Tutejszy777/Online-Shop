4dadd786779547f9c1d5fe26d5ab0695e50e8705
Patrik <Muravski.patrik@gmail.com>
2025-03-31 15:49:06 +0300

    added appUser (unfacntional)

---
+++ MutipleStoreWebApp/Data/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MutipleStoreWebApp.Data
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly LastLoggin { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public int? StoreId { get; set; }  // Null for Admin
+        public Store Store { get; set; }
+    }
+}
