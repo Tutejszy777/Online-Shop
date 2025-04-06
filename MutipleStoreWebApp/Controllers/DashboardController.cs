d5cf1aaac9a9e7baf66ca52997e18b0f607bac5a
Patrik <Muravski.patrik@gmail.com>
2025-04-06 12:06:42 +0300

    autorization for dashboard and layout changed

--- MutipleStoreWebApp/Controllers/DashboardController.cs
+++ MutipleStoreWebApp/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MutipleStoreWebApp.Controllers
 {
+    [Authorize(Roles = "Admin,Owner")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
