d2db439ce124a0f479f7b67297d2c7ffb7ca3b9b
Patrik <Muravski.patrik@gmail.com>
2025-04-06 20:47:51 +0300

    added route configuration

--- MutipleStoreWebApp/Program.cs
+++ MutipleStoreWebApp/Program.cs
@@ -38,8 +38,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "ShopRoute",
+    pattern: "{shopSlug}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
