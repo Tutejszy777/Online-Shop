538aba7305d4c18f5ee8182a10c2a7d144fc2c8d
Patrik <Muravski.patrik@gmail.com>
2025-04-10 12:23:08 +0300

    added a product service

---
+++ MutipleStoreWebApp/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MutipleStoreWebApp.Data;
+
+namespace MutipleStoreWebApp.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetProductsByStoreId(int storeId)
+        {
+            return await _context.Products
+                .Where(p => p.StoreId == storeId)
+                .ToListAsync();
+        }
+    }
+}
