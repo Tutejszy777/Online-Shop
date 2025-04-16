3a277bfc6e2dcd56124400998217a55ecb6e9eba
Patrik <Muravski.patrik@gmail.com>
2025-04-16 14:09:30 +0300

    addded a product with for search by categorie

---
+++ MutipleStoreWebApp/Models/ProductCategorieVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MutipleStoreWebApp.Data;
+
+namespace MutipleStoreWebApp.Models
+{
+    public class ProductCategorieVM
+    {
+        public List<Product>? Products { get; set; }
+        public SelectList Categories { get; set; }
+        public string? Categorie { get; set; }
+        public string? searchString { get; set; }
+    }
+}
