d4a7a23d24b0f63033dfffd6973f8473a04a1b2e
Patrik <Muravski.patrik@gmail.com>
2025-04-19 18:31:21 +0300

    added DbInitializer for future

---
+++ MutipleStoreWebApp/Data/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿namespace MutipleStoreWebApp.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Stores.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+
+        }
+    }
+}
