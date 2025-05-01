e09a0583549da63efed5d2ef8036466e0e84c34d
Patrik <Muravski.patrik@gmail.com>
2025-05-01 22:55:25 +0300

    unfinished service and table

--- MutipleStoreWebApp/Data/Order.cs
+++ MutipleStoreWebApp/Data/Order.cs
@@ -3,5 +3,17 @@
     public class Order
     {
         public int Id { get; set; }
+
+        public Guid AppUserId { get; set; }
+        public AppUser? AppUser { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+        public DateTime? Created { get; set; } = default(DateTime?);
+
+        public bool StatusConnfirmed { get; set; }
+
+        public decimal? Total { get; set; }
+
+        
     }
 }
