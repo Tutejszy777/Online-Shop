9f06ec197e4ff252cd8accb176f6424f536d1bc9
Patrik <Muravski.patrik@gmail.com>
2025-05-04 08:47:33 +0300

    order table seems to be ready

--- MutipleStoreWebApp/Data/Order.cs
+++ MutipleStoreWebApp/Data/Order.cs
@@ -1,10 +1,19 @@
-﻿using MutipleStoreWebApp.Data.Static;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MutipleStoreWebApp.Data.Static;
 
 namespace MutipleStoreWebApp.Data
 {
+
     public class Order
     {
         public int Id { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+        public Status Status { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Total { get; set; }
 
         public Guid AppUserId { get; set; }
         public AppUser? AppUser { get; set; }
@@ -12,12 +21,10 @@ namespace MutipleStoreWebApp.Data
         public int StoreId { get; set; }
         public Store? Store { get; set; }
 
-        public List<Product> Products { get; set; } = new List<Product>();
-        public DateTime? Created { get; set; } = default(DateTime?);
 
-        public Status Status { get; set; } 
-
-        public decimal? Total { get; set; }
-
+        public virtual ICollection<Shipment> Shipments { get; set; } = new List<Shipment>();
+        public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+        public virtual Invoice Invoice { get; set; }
     }
 }
